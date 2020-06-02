dcf7fe3bfc3ac8a1fb6b6ffb27ecc260b9778b36
chenying <chenying@wowkai.cn>
2020-06-02 23:07:33 +0800

    我的主页以页面完成

--- src/pages/personal/personal.js
+++ src/pages/personal/personal.js
@@ -1,7 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-02 22:31:31
+ * @LastEditTime: 2020-06-02 22:55:27
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /xcx-appointment-mf-user-sass/src/pages/personal/personal.js
+ */
 const api = require("../../api/personal");
 let app = getApp();
 Page({
   data: {
+
   },
   onLoad: function (options) {},
 });
